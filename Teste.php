50bedeb09c3a70b9d8071a41dce25f0ac22799b1
João Gabriel Pancheski <i22111@integrado.int>
2019-09-19 14:18:03 -0300

    First commit

---
+++ Teste.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class Teste
+{
+    public static function metodo () {
+        echo "teste";
+    }
+
+}
+
+class Teste2
+{
+    public static function metodo () {
+        echo "teste";
+    }
+
+}
